89dbb02b8dd2545a8774fa3518470c9fde28f49f
GoldenSpade <GoldenSpade@proton.me>
2025-12-03 02:00:47 +0200

    Clean code in useSmoothScroll.js

--- src/composables/useSmoothScroll.js
+++ src/composables/useSmoothScroll.js
@@ -3,15 +3,13 @@
  */
 import Lenis from 'lenis'
 
-let lenisInstance = null
-
 /**
  * Инициализирует Lenis для плавной прокрутки
  * @param {Object} options - Опции конфигурации Lenis
  */
 export function initSmoothScroll(options = {}) {
   // Создаём экземпляр Lenis с настройками
-  lenisInstance = new Lenis({
+  const lenisInstance = new Lenis({
     duration: 1.2,           // Длительность прокрутки (чем больше, тем медленнее)
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easing функция
     orientation: 'vertical', // Вертикальная прокрутка
@@ -49,48 +47,4 @@ export function initSmoothScroll(options = {}) {
   })
 
   return lenisInstance
-}
-
-/**
- * Устанавливает параметры плавности прокрутки
- * @param {Object} options - Опции для настройки
- * @param {number} options.duration - Длительность анимации прокрутки (default: 1.2)
- * @param {number} options.wheelMultiplier - Множитель скорости колёсика (default: 1)
- */
-export function setScrollOptions(options) {
-  if (lenisInstance) {
-    if (options.duration !== undefined) {
-      lenisInstance.options.duration = options.duration
-    }
-    if (options.wheelMultiplier !== undefined) {
-      lenisInstance.options.wheelMultiplier = options.wheelMultiplier
-    }
-  }
-}
-
-/**
- * Получает экземпляр Lenis
- * @returns {Lenis} - Экземпляр Lenis
- */
-export function getLenisInstance() {
-  return lenisInstance
-}
-
-/**
- * Уничтожает экземпляр Lenis
- */
-export function destroySmoothScroll() {
-  if (lenisInstance) {
-    lenisInstance.destroy()
-    lenisInstance = null
-  }
-}
-
-export function useSmoothScroll() {
-  return {
-    initSmoothScroll,
-    setScrollOptions,
-    getLenisInstance,
-    destroySmoothScroll
-  }
 }
