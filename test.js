b8f6c65b28ca94093b8d6efedd87a2475609c46f
GoldenSpade <GoldenSpade@proton.me>
2025-12-08 21:30:04 +0200

    .env.example

---
+++ test.js
@@ -0,0 +1,105 @@
+const obj = {
+  ok: true,
+  result: [
+    {
+      update_id: 5162817,
+      message: {
+        message_id: 338,
+        from: {
+          id: 7606207943,
+          is_bot: false,
+          first_name: 'Vlad',
+          username: 'irobot_01',
+          language_code: 'ru',
+        },
+        chat: { id: 7606207943, first_name: 'Vlad', username: 'irobot_01', type: 'private' },
+        date: 1765196704,
+        text: '/start',
+        entities: [{ offset: 0, length: 6, type: 'bot_command' }],
+      },
+    },
+    {
+      update_id: 5162818,
+      my_chat_member: {
+        chat: { id: 7606207943, first_name: 'Vlad', username: 'irobot_01', type: 'private' },
+        from: {
+          id: 7606207943,
+          is_bot: false,
+          first_name: 'Vlad',
+          username: 'irobot_01',
+          language_code: 'ru',
+        },
+        date: 1765203260,
+        old_chat_member: {
+          user: {
+            id: 7851397475,
+            is_bot: true,
+            first_name: 'TesterBot',
+            username: 'TestOfMorfejBot',
+          },
+          status: 'member',
+        },
+        new_chat_member: {
+          user: {
+            id: 7851397475,
+            is_bot: true,
+            first_name: 'TesterBot',
+            username: 'TestOfMorfejBot',
+          },
+          status: 'kicked',
+          until_date: 0,
+        },
+      },
+    },
+    {
+      update_id: 5162819,
+      my_chat_member: {
+        chat: { id: 7606207943, first_name: 'Vlad', username: 'irobot_01', type: 'private' },
+        from: {
+          id: 7606207943,
+          is_bot: false,
+          first_name: 'Vlad',
+          username: 'irobot_01',
+          language_code: 'ru',
+        },
+        date: 1765203264,
+        old_chat_member: {
+          user: {
+            id: 7851397475,
+            is_bot: true,
+            first_name: 'TesterBot',
+            username: 'TestOfMorfejBot',
+          },
+          status: 'kicked',
+          until_date: 0,
+        },
+        new_chat_member: {
+          user: {
+            id: 7851397475,
+            is_bot: true,
+            first_name: 'TesterBot',
+            username: 'TestOfMorfejBot',
+          },
+          status: 'member',
+        },
+      },
+    },
+    {
+      update_id: 5162820,
+      message: {
+        message_id: 341,
+        from: {
+          id: 7606207943,
+          is_bot: false,
+          first_name: 'Vlad',
+          username: 'irobot_01',
+          language_code: 'ru',
+        },
+        chat: { id: 7606207943, first_name: 'Vlad', username: 'irobot_01', type: 'private' },
+        date: 1765203264,
+        text: '/start',
+        entities: [{ offset: 0, length: 6, type: 'bot_command' }],
+      },
+    },
+  ],
+}
